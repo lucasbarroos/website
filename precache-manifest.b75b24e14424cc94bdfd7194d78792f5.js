fe3f4f9e920ac11cef1852266e82b193456f1992
Lucas Barros <lbarros.comp@gmail.com>
2020-06-19 02:01:50 -0300

    Updates

---
+++ precache-manifest.b75b24e14424cc94bdfd7194d78792f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d5754d19d52ae54091a5455c1be9767",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "9dde2d0862d065382669",
+    "url": "/website/static/css/main.6d67e516.chunk.css"
+  },
+  {
+    "revision": "6c0b8fbc96ddd0388f31",
+    "url": "/website/static/js/2.fb311e87.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/website/static/js/2.fb311e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dde2d0862d065382669",
+    "url": "/website/static/js/main.f0345628.chunk.js"
+  },
+  {
+    "revision": "fd2c455953733fae8441",
+    "url": "/website/static/js/runtime-main.509a91f0.js"
+  }
+]);
