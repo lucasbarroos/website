602e3d6201bfc02752f87422efd994c30a95963d
Lucas Barros <lbarros.comp@gmail.com>
2020-06-19 01:46:15 -0300

    Finishing the main layout for the website

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
 import React, { useState } from 'react';
+import { Grid } from '@material-ui/core';
+import { Link } from 'react-scroll';
 import Lottie from 'react-lottie';
 import {
     Container,
+    Content,
+    FirstWave,
     Name,
     Subname,
     Description,
@@ -9,38 +14,105 @@ import {
     ButtonBox,
     AboutLink,
     ContactLink,
+    AboutContainer,
+    AboutTitle,
+    AboutAnimation,
+    AboutText,
+    ContactContainer,
+    SecondWave,
+    ContactLogo,
+    ContactContent,
+    LogoTitle,
+    ContactItem,
+    ContactTitle,
 } from './styles';
 
-import animationData from '../../assets/animations/developer.json';
-
+import animationDeveloper from '../../assets/animations/developer.json';
+import animationGirl from '../../assets/animations/girl.json';
+const githubIcon = 'https://github.githubassets.com/images/modules/logos_page/Octocat.png';
+const linkedinIcon = 'https://www.spiner.com.br/wp-content/uploads/2019/02/midias-sociais-linkedin-icon.png';
 
 const Home = () => {
-    const [defaultOptions] = useState({
-      loop: true,
-      autoplay: true, 
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice'
-      }
+    const [developerLottie] = useState({
+        loop: true,
+        autoplay: true,
+        animationData: animationDeveloper,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
+    });
+
+    const [girlLottie] = useState({
+        loop: true,
+        autoplay: true,
+        animationData: animationGirl,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice'
+        }
     });
 
     return (
         <>
-            <Container>
-                <Name>Lucas</Name>
-                <Subname>Barros</Subname>
-                <Description>I'm Fullstack Developer</Description>        
-                <Animation>
-                    <Lottie options={defaultOptions} isClickToPauseDisabled/>
-                </Animation>
-                <ButtonBox>
-                    <AboutLink>About</AboutLink>
-                    <ContactLink>Contact</ContactLink>
-                </ButtonBox>
+            <Container id="home">
+                <Content>
+                    <Name>Lucas</Name>
+                    <Subname>Barros</Subname>
+                    <Description>I'm Fullstack Developer</Description>        
+                    <Animation>
+                        <Lottie options={developerLottie} isClickToPauseDisabled/>
+                    </Animation>
+                    <ButtonBox>
+                        <AboutLink>
+                            <Link activeClass="active" to="about" spy={true} smooth="easeInOutQuint" offset={50} duration={1000}>
+                                About
+                            </Link>
+                        </AboutLink>
+                        <ContactLink>
+                            <Link activeClass="active" to="contact" smooth="easeInOutQuint" spy={true}  offset={50} duration={1000}>
+                                Contact
+                            </Link>
+                        </ContactLink>
+                    </ButtonBox>
+                    <FirstWave>
+                        <svg viewBox="0 0 1166 402" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M0 283.817L38.8667 272.065C77.7333 259.426 155.467 237.253 233.2 248.34C310.933 259.426 388.667 308.207 466.4 307.542C544.133 308.207 621.867 259.426 699.6 283.817C777.333 308.207 855.067 401.335 932.8 402C1010.53 401.335 1088.27 308.207 1127.13 260.092L1166 212.863V0H1127.13C1088.27 0 1010.53 0 932.8 0C855.067 0 777.333 0 699.6 0C621.867 0 544.133 0 466.4 0C388.667 0 310.933 0 233.2 0C155.467 0 77.7333 0 38.8667 0H0V283.817Z" fill="#4A64E9"/>
+                        </svg>
+                    </FirstWave>
+                </Content>
             </Container>
-            <svg viewBox="0 0 1166 402" style={{ marginTop: -30 }} fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0 283.817L38.8667 272.065C77.7333 259.426 155.467 237.253 233.2 248.34C310.933 259.426 388.667 308.207 466.4 307.542C544.133 308.207 621.867 259.426 699.6 283.817C777.333 308.207 855.067 401.335 932.8 402C1010.53 401.335 1088.27 308.207 1127.13 260.092L1166 212.863V0H1127.13C1088.27 0 1010.53 0 932.8 0C855.067 0 777.333 0 699.6 0C621.867 0 544.133 0 466.4 0C388.667 0 310.933 0 233.2 0C155.467 0 77.7333 0 38.8667 0H0V283.817Z" fill="#4A64E9"/>
-            </svg>  
+            <AboutContainer id="about">
+                <AboutTitle>About me</AboutTitle>                      
+                <AboutAnimation>
+                    <Lottie options={girlLottie} isClickToPauseDisabled/>
+                </AboutAnimation>
+                <AboutText>Hello, I'm a Full Stack Developer with 6 years of experience. I work from project management to final delivery. Dedicated and I like challenges!</AboutText>
+            </AboutContainer>
+            <ContactContainer>
+                <SecondWave>
+                    <svg viewBox="0 0 1165 288" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 64L19.4333 53.3C38.8667 43 77.7333 21 116.6 42.7C155.467 64 194.333 128 233.2 138.7C272.067 149 310.933 107 349.8 96C388.667 85 427.533 107 466.4 133.3C505.267 160 544.133 192 583 181.3C621.867 171 660.733 117 699.6 101.3C738.467 85 777.333 107 816.2 117.3C855.067 128 893.933 128 932.8 128C971.667 128 1010.53 128 1049.4 106.7C1088.27 85 1127.13 43 1146.57 21.3L1166 0V288H1146.57C1127.13 288 1088.27 288 1049.4 288C1010.53 288 971.667 288 932.8 288C893.933 288 855.067 288 816.2 288C777.333 288 738.467 288 699.6 288C660.733 288 621.867 288 583 288C544.133 288 505.267 288 466.4 288C427.533 288 388.667 288 349.8 288C310.933 288 272.067 288 233.2 288C194.333 288 155.467 288 116.6 288C77.7333 288 38.8667 288 19.4333 288H0V64Z" fill="#4A64E9"/>
+                    </svg>
+                </SecondWave>
+                <ContactContent id="contact">
+                    <ContactTitle>Contact</ContactTitle>
+                    <Grid container>
+                        <Grid item lg={3} />
+                        <Grid item lg={3} md={6} xs={12} sm={12} align="center">
+                            <ContactItem>
+                                <ContactLogo src={githubIcon} />
+                                <LogoTitle>Github</LogoTitle>
+                            </ContactItem>
+                        </Grid>
+                        <Grid item lg={3} md={6} xs={12} sm={12} align="center">
+                            <ContactItem>
+                                <ContactLogo src={linkedinIcon} style={{marginBottom: 0}} />
+                                <LogoTitle>Linkedin</LogoTitle>
+                            </ContactItem>
+                        </Grid>
+                        <Grid item lg={3} />
+                    </Grid>
+                </ContactContent>
+            </ContactContainer>
         </>
     )
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,44 +2,62 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     position: relative;
-    height: 70vh;
+    height: 65vh;
     background-color: #4A64E9;
+    margin-bottom: 25vh;
+`;
+
+export const Content = styled.div`
+    position: relative;
+`;
+
+export const FirstWave = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 20vh;
+    z-index: 500;
+    bottom: -60vh;
 `;
 
 export const Name = styled.span`
     position: absolute;
     left: 20vw;
-    top: 30vh;
+    top: 20vh;
+    z-index: 1000;
     font-size: 9vw;
     font-weight: 500;
     color: #ffffff;
     margin-top: 20px;
+    filter: blur(0.5px);
 `;
 
 export const Subname = styled.span`
     position: absolute;
     left: 20vw;
-    top: 45vh;
+    top: 35vh;
+    z-index: 1000;
     font-size: 9vw;
     font-weight: 500;
     color: #ffffff;
     margin-top: 20px;
+    filter: blur(0.5px);
 `;
 
 export const Description = styled.span`    
     position: absolute;
-    bottom: -12vh;
+    top: 55vh;
+    z-index: 1000;
     right: 15vw;
     font-size: 3vw;
     color: #9BABF3;
 `;
 
-
 export const Animation = styled.div`
     height: 60vh;
     width: 40vw;
+    z-index: 1000;
     position: absolute;
-    top: 10vh;
+    top: 0vh;
     right: 10vw;
 `;
 
@@ -53,8 +71,6 @@ export const ButtonBox = styled.div`
     width: 6%;
     top: -20px;
     height: 120vh;
-    border: 0px solid white;
-    box-shadow: 2px 2px 8px white;
 `;
 
 export const AboutLink = styled.span`   
@@ -62,16 +78,110 @@ export const AboutLink = styled.span`
     color: white;
     transform: rotate(-90deg);
     margin-left: 1vw;
-    margin-top: -20vh;
+    margin-top: -35vh;
     margin-bottom: 15vh;
+    z-index: 1000;
     cursor: pointer;
+    opacity: 90%;
+    filter: blur(0.7px);
 `;
 
 export const ContactLink = styled.span`
     font-size: 2vw;
     color: white;
     transform: rotate(-90deg);
-    margin-top: 10vh;
+    margin-top: 5vh;
     margin-left: 0vw;
+    z-index: 2000;
     cursor: pointer;
+`;
+
+export const AboutContainer = styled.div`
+    position: relative;
+    min-height: 80vh;
+    width: 100%;
+    background-color: white;
+`;
+
+export const AboutTitle = styled.div`
+    position: absolute;
+    font-size: 7vw;
+    color: #9BABF3;
+    transform: rotate(-90deg);
+    left: -10vw;
+    top: 25vh;
+    font-weight: 600;
+`;
+
+export const AboutAnimation = styled.div`
+    height: 35vh;
+    width: 45vw;
+    position: absolute;
+    top: 5vh;
+    left: 30vw;
+`;
+
+export const AboutText = styled.span`
+    width: 45vw;
+    font-size: 1.8vw;<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+    text-align: justify;
+    position: absolute;
+    top: 40vh;
+    left: 30vw;
+    color: #172F94;
+    font-weight: 600;
+`;
+
+export const SecondWave = styled.div`
+    position: absolute;
+    top: -40px;
+    top: -35vh;
+    width: 100%;
+    height: 10vh;
+`;
+
+export const ContactContainer = styled.div`
+    margin-top: 25vh;
+    position: relative;
+    background-color: #4A64E9;
+    height: 40vh;
+`;
+
+export const ContactContent = styled.div`
+    position: absolute;
+    top: 40px;
+    z-index: 1000;
+    width: 100%;
+`;
+
+export const ContactItem = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+export const ContactLogo = styled.img`
+    height: 6vw;
+    width: 6vw;
+    margin-bottom: 10px;
+    cursor: pointer;
+`;
+
+export const LogoTitle = styled.a`
+    color: white;
+    font-size: 1.1vw;
+    font-weight: 600;
+    cursor: pointer;
+`;
+
+export const ContactTitle = styled.div`
+    position: absolute;
+    font-size: 7vw;
+    color: #ffffff;
+    transform: rotate(-90deg);
+    left: -7vw;
+    top: 0vh;
+    font-weight: 600;
 `;
